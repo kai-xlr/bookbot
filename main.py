86a6bfa2a05547100e70c6ace370032d31793fa2
kai-xlr <xhauma@protonmail.com>
2025-01-23 17:44:26 -0500

    Fixed the word_list variable for uniformity

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def main():
     print(f"{num_words} words found in the book.")
     
 def word_count(subject: list[str]) -> list[str]:
-    wordlist = len(subject.split())
-    return wordlist
+    word_list = len(subject.split())
+    return word_list
 
 def get_book_text(path: list[str]) -> list[str]:
     with open(path) as book:
